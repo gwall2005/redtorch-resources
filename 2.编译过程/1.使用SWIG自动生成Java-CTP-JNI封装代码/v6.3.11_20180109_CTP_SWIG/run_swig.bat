69f35d3c26172bdf1d0610989fee12d3268ffae0
sun0x00 <sun0x00@gmail.com>
2019-05-16 17:28:45 +0800

    v0.2首次提交

--- 2.编译过程/1.使用SWIG自动生成Java-CTP-JNI封装代码/v6.3.11_20180109_CTP_SWIG/run_swig.bat
+++
@@ -1,8 +0,0 @@
-swig -c++ -java -package xyz.redtorch.api.jctp.td -outdir ./java_src/td -I../include jctptraderapiv6v3v11x64.i
-
-
-swig -c++ -java -package xyz.redtorch.api.jctp.md -outdir ./java_src/md -I../include jctpmdapiv6v3v11x64.i
-
-
-
-pause
